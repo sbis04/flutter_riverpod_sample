49236b6176cab9fcd75e7eac772c8fef5b310199
Souvik Biswas <sbis1999@gmail.com>
2021-02-23 17:35:03 +0530

    Added riverpod to pass the string :rocket:

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 TextStyle textStyle = TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold);
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+final welcomeProvider = Provider((ref) => 'Welcome to Riverpod');
+
+class MyApp extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+    final welcomeString = watch(welcomeProvider);
+
     return MaterialApp(
       title: 'Riverpod Sample',
       theme: ThemeData(
@@ -17,7 +26,7 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         body: Center(
-          child: Text('Hello', style: textStyle),
+          child: Text(welcomeString, style: textStyle),
         ),
       ),
     );
